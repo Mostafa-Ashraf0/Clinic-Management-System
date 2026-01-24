39ea2dc48b594eb611b77f99082e3f2c9fd850ee
Mostafa-Ashraf0 <mostafaashrof158@gmail.com>
2026-01-24 11:24:57 +0200

    medical info edit

--- src/components/EmrMedicalInfo.jsx
+++ src/components/EmrMedicalInfo.jsx
@@ -1,17 +1,14 @@
 import style from '../assets/emrMedicalInfo.module.css';
-import VitaminDCard from './VitaminDCard';
-import TSScoreCard from './TSScoreCard';
 import CalPhoCard from './CalPhoCard';
-import InflammatoryMarkersCard from './InflaMarkersCard';
 const EmrMedicalInfo = ()=>{
     return(
         <div className={style.medical_info}>
-            <span className={style.title}>Medical Information</span>
+            <div className={style.head}>
+                <span className={style.title}>Medical Tests</span>
+                <button>Add New Test</button>
+            </div>
             <div className={style.info_cards}>
-                <TSScoreCard/>
-                <VitaminDCard/>
                 <CalPhoCard/>
-                <InflammatoryMarkersCard/>
             </div>
         </div>
     )
