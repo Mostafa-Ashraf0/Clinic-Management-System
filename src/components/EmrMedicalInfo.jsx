71a87b16b2d2b9c0233b8a5979d0070e556c12c1
Mostafa-Ashraf0 <mostafaashrof158@gmail.com>
2026-02-03 18:19:24 +0200

    getTestsByPatient rpc

--- src/components/EmrMedicalInfo.jsx
+++ src/components/EmrMedicalInfo.jsx
@@ -3,12 +3,27 @@ import PatientTestCard from './PatientTestCard';
 import MedicalTestRecordForm from './medicalTests/MedicalTestRecordForm';
 import { useDispatch } from 'react-redux';
 import { setIsVisible } from '../features/emr/testRecordFormSlice';
+import { getTestsByPatientId } from '../features/emr/getTestsByPatientId';
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 const EmrMedicalInfo = ()=>{
+    const [tests, setTests] = useState([]);
+    const {patientId} = useParams();
     const dispatch = useDispatch();
     const handleClick = ()=>{
         dispatch(setIsVisible(true));
     }
+    const getTestData = async()=>{
+        const tests = await getTestsByPatientId(patientId);
+        if(tests) setTests(tests);
+    }
+    useEffect(()=>{
+        getTestData();
+    },[]);
+    useEffect(()=>{
+        console.log(tests);
+    },[tests])
     return(
         <div className={style.medical_info}>
             <div className={style.head}>
---
+++ src/features/emr/getTestsByPatientId.js
@@ -0,0 +1,20 @@
+import supabase from "../../utils/supabase";
+import { toast } from "react-toastify";
+
+const getTestsByPatientId = async (id) => {
+  try {
+    const { data, error } = await supabase
+      .rpc('get_latest_tests_with_values', {
+        p_patient_id: id
+      });
+
+    if (error) throw error;
+    return data;
+  } catch (error) {
+    console.log(error);
+    toast.error('something went wrong while fetching medical test');
+  }
+};
+
+
+export {getTestsByPatientId};
