e64ffd4fc1e035f3c44abc18f10ee5a778d886fc
mostafa ashraf <mostafaashrof158@gmail.com>
2026-03-03 23:22:13 +0200

    time slots

--- src/components/LiveDashboard/Dashboard.jsx
+++ src/components/LiveDashboard/Dashboard.jsx
@@ -3,7 +3,7 @@ const generateSlot = ()=>{
     const timeSlots = [];
     let start_time = 17 * 60;
     const end_time = 24 * 60;
-    const duration = 20;
+    const duration = 15;
     while(start_time<end_time){
         const hours = Math.floor(start_time/60);
         const minutes = start_time % 60;
@@ -24,10 +24,34 @@ const Dashboard = ()=>{
         <h2 className={style.dashboardTitle}>Today's Appointments</h2>
 
         <div className={style.slotsContainer}>
+            <div className={style.slotHead}>
+                <div className={style.main}>
+                    <div className={style.timeContainer}>
+                        <span className={style.time}>Time</span>
+                    </div>
+                    <div className={style.info}>
+                        <span className={style.patient}>Patient Name</span>
+                    </div>
+                </div>
+                <div className={style.tags}>
+                    <span className={style.type}>Session type</span>
+                    <span className={style.status}>Status</span>
+                </div>
+            </div>
             {timeSlots.map((time, index) => (
             <div key={index} className={style.slot}>
-                <span className={style.time}>{time}</span>
-                <span className={style.status}>Available</span>
+                <div className={style.main}>
+                    <div className={style.timeContainer}>
+                        <span className={style.time}>{time}</span>
+                    </div>
+                    <div className={style.info}>
+                        <span className={style.patient}>Mostafa Ashraf</span>
+                    </div>
+                </div>
+                <div className={style.tags}>
+                    <span className={style.type}>Checkup</span>
+                    <span className={style.status}>Available</span>
+                </div>
             </div>
             ))}
         </div>
